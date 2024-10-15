4a991c1f09ea4f7e78dba9adc6c9da0424ab9a74
moose1108 <75203439+moose1108@users.noreply.github.com>
2024-10-15 13:54:28 +0800

    Update inference.py

--- inference.py
+++ inference.py
@@ -13,7 +13,9 @@ import tqdm
 
 parser = argparse.ArgumentParser(description='Run climate model emulation.')
 parser.add_argument('--variables', nargs='+', default=['q700'], help='List of variables')
-parser.add_argument('--predictand', type=str, default='pr')
+parser.add_argument('--predictand', type=str, default='RAINNC')
+parser.add_argument('--start_year', type=str, default='', help='start year')
+parser.add_argument('--end_year', type=str, default='', help='end year')
 parser.add_argument('--topology', type=str, default='deepesd')
 parser.add_argument('--approach', type=str, default='MOS-E')
 parser.add_argument('--outputFileName', type=str, default='./predictions/a.nc')
@@ -28,6 +30,8 @@ variables = args.variables
 predictand = args.predictand
 topology = args.topology
 approach = args.approach
+start_year = args.start_year
+end_year = args.end_year
 outputFileName = args.outputFileName
 scale = args.scale
 bias_correction = args.bias_correction
@@ -41,19 +45,18 @@ print(f'y label: {predictand}')
 print(f'years to predict: {years}')
 print(f'ModelName: {modelPath}')
 print(f'scale: {scale}')
-a = input()
 
 def is_leap_year(year):
     return year % 4 == 0 and (year % 100 != 0 or year % 400 == 0)
 
 print('===== concat predictand =====')
-predictand_base_path = '/work/moose1108/corrdiff-like/data/02-predictand_TReAD/RAINNC/'
-y_datasets = []
-for year in tqdm.tqdm(range(2001, 2010, 2)):
-    file_path = f'{predictand_base_path}/TReAD_daily_{year}_RAINNC.nc'
+predictand_base_path = f'/work/moose1108/corrdiff-like/data/02-predictand_TReAD/{predictand}/'
+yearly_ys = []
+for year in tqdm.tqdm(range(int(start_year), int(end_year)+1)):
+    file_path = f'{predictand_base_path}TReAD_daily_{year}_{predictand}.nc'
     dataset = xr.open_dataset(file_path)
-    y_datasets.append(dataset)
-y = xr.concat(y_datasets, dim='Day')
+    yearly_ys.append(dataset)
+y = xr.concat(yearly_ys, dim='Day')
 min_lat = y['Lat'].min().item()
 max_lat = y['Lat'].max().item()
 min_lon = y['Lon'].min().item()
@@ -63,37 +66,43 @@ print(f'longitude range: {min_lon}~{max_lon}')
 
 print('===== concat predictor base data =====')
 base_path = '/work/moose1108/corrdiff-like/data/01-predictor_ERA5/'
-x_datasets = {}
 
+data_values = {var: [] for var in variables}
+coordinates = {'time': [], 'latitude': None, 'longitude': None}
 for var in variables:
     monthly_datasets = []
-    for month in range(1, 13):
-        file_path = f'{base_path}{var}/{years}/ERA5_PRS_{var}_{years}{month:02}_r1440x721_day.nc'
-        dataset = xr.open_dataset(file_path)
-        monthly_datasets.append(dataset)
-    concatenated_dataset = xr.concat(monthly_datasets, dim='time')
-    x_datasets[var] = concatenated_dataset
-x = xr.merge([x_datasets[var] for var in variables])
-
-base_datasets = {}
-for var in variables:
-    monthly_datasets = []
-    for year in tqdm.tqdm(range(2001, 2010, 2)):
+    for year in tqdm.tqdm(range(int(years), int(years)+1)):
         for month in range(1, 13):
             file_path = f'{base_path}{var}/{str(year)}/ERA5_PRS_{var}_{str(year)}{month:02}_r1440x721_day.nc'
             dataset = xr.open_dataset(file_path)
             if is_leap_year(year) and month == 2:
                 dataset = dataset.sel(time=~((dataset.time.dt.month == 2) & (dataset.time.dt.day == 29)))
             dataset = dataset.sel(latitude=slice(max_lat, min_lat), longitude=slice(min_lon, max_lon))
-            monthly_datasets.append(dataset)
-    concatenated_dataset = xr.concat(monthly_datasets, dim='time')
-    base_datasets[var] = concatenated_dataset
-base = xr.merge([base_datasets[var] for var in variables])
 
+            if coordinates['latitude'] is None:
+                coordinates['latitude'] = dataset['latitude'].values.tolist()
+                coordinates['longitude'] = dataset['longitude'].values.tolist()
+            if var == variables[0]:
+                coordinates['time'].extend(dataset['time'].to_index().tolist())
+            data_values[var].append(dataset[var].values)
+            dataset.close()
+
+for var in data_values:
+    data_values[var] = np.concatenate(data_values[var], axis=0)
+
+new_data_arrays = {var: xr.DataArray(data=data_values[var],
+                                     dims=['time', 'latitude', 'longitude'],
+                                     coords={'time': coordinates['time'],
+                                             'latitude': coordinates['latitude'],
+                                             'longitude': coordinates['longitude']})
+                   for var in variables}
+
+x = xr.Dataset(new_data_arrays)
+
+base = xr.open_dataset(f'/work/moose1108/corrdiff-like/data/{start_year}_{end_year}.nc')
+base = base[variables]
 base = base.sel(latitude=slice(max_lat, min_lat), longitude=slice(min_lon, max_lon))
-base = base.drop_vars('time_bnds')
 x = x.sel(latitude=slice(max_lat, min_lat), longitude=slice(min_lon, max_lon))
-x = x.drop_vars('time_bnds')
 
 if scale is True:
     print('scaling...')
@@ -118,30 +127,29 @@ if topology == 'deepesd':
     mask_Onedim = mask.landmask.values.reshape((np.prod(mask.landmask.shape)))
     ind = [i for i in range(len(mask_Onedim)) if mask_Onedim[i] == 1]
     pred = reshapeToMap(grid = pred, ntime = x.dims['time'], nlat = mask.dims['Lat'], nlon = mask.dims['Lon'], indLand = ind)
+
 if topology == 'unet':
     sea = mask.sftlf.values == 0
     pred = np.squeeze(pred)
     pred[:,sea] = np.nan
 
 if predictand == 'RAINNC':
-    y_bin = binaryGrid(y.RAINNC, condition = 'GE', threshold = 1)
-    ## Prediction on the train set -----------
-    base2 = scaleGrid(base, base = base, type = 'standardize', spatialFrame = 'gridbox')
-    base_array = base2.to_stacked_array("var", sample_dims = ["longitude", "latitude", "time"]).values
-    pred_ocu_train = model.predict(base_array)[:,:,0]
-    pred_ocu_train = reshapeToMap(grid = pred_ocu_train, ntime = base2.dims['time'], nlat = mask.dims['Lat'], nlon = mask.dims['Lon'], indLand = ind)
-    pred_ocu_train = xr.DataArray(pred_ocu_train, dims = ['time','latitude','longitude'], coords = {'longitude': mask.Lon.values, 'latitude': mask.Lat.values, 'time': y.Day.values})
-    ## ---------------------------------------
-    ## Recovering the complete serie -----------
+    # y_bin = binaryGrid(y.RAINNC, condition = 'GE', threshold = 1)
+    # ## Prediction on the train set -----------
+    # base2 = scaleGrid(base, base = base, type = 'standardize', spatialFrame = 'gridbox')
+    # base_array = base2.to_stacked_array("var", sample_dims = ["longitude", "latitude", "time"]).values
+    # pred_ocu_train = model.predict(base_array)[:,:,0]
+    # pred_ocu_train = reshapeToMap(grid = pred_ocu_train, ntime = base2.dims['time'], nlat = mask.dims['Lat'], nlon = mask.dims['Lon'], indLand = ind)
+    # pred_ocu_train = xr.DataArray(pred_ocu_train, dims = ['time','latitude','longitude'], coords = {'longitude': mask.Lon.values, 'latitude': mask.Lat.values, 'time': base.time.values})
+    # ## Recovering the complete serie -----------
     pred = xr.Dataset(data_vars = {'p': (['time','latitude','longitude'], pred[:,:,:,0]),
                                     'log_alpha': (['time','latitude','longitude'], pred[:,:,:,1]),
                                     'log_beta': (['time','latitude','longitude'], pred[:,:,:,2])},
                                     coords = {'longitude': mask.Lon.values, 'latitude': mask.Lat.values, 'time': x.time.values})
-    pred_bin = adjustRainfall(grid = pred.p, refPred = pred_ocu_train, refObs = y_bin)
+    pred_bin = pred.p#adjustRainfall(grid = pred.p, refPred = pred_ocu_train, refObs = y_bin)
     pred_amo = computeRainfall(log_alpha = pred.log_alpha, log_beta = pred.log_beta, bias = 1, simulate = True)
     pred = pred_bin * pred_amo
     pred = pred.values
-    ## -----------------------------------------
 
 template_predictand = xr.open_dataset('/work/moose1108/corrdiff-like/data/02-predictand_TReAD/RAINNC/TReAD_daily_2009_RAINNC.nc')
 pred = xr.Dataset(
@@ -149,5 +157,7 @@ pred = xr.Dataset(
     coords = {'longitude': template_predictand.Lon.values, 'latitude': template_predictand.Lat.values, 'time': x.time.values},
     attrs = description
 )
+pred = pred / 24
 print(pred)
+
 pred.to_netcdf(outputFileName)
